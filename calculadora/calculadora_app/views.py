7cffba7826c6bc19cb1acc59bcd80828e25f56e5
Angelito50 <angelmendrano503@gmail.com>
2024-06-12 19:31:52 -0600

    mas cambios

--- calculadora/calculadora_app/views.py
+++ calculadora/calculadora_app/views.py
@@ -1,24 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-from django.template import Template, Context
-from .models import *
-import numpy as np
-import sympy as sym
-import matplotlib.pyplot as plt
 
 def Procedimiento(request):
-    xi = tbl_Ecuaciones()
-    fi = tbl_Ecuaciones()
+    resultado = None
     
     if request.method == 'POST':
-        # Obtener los datos del formulario POST y guardar en la base de datos
+        xi = request.POST.get('xi_valores', '')  # Obtener el valor de xi_valores del formulario
+        fi = request.POST.get('fi_valores', '')  # Obtener el valor de fi_valores del formulario
         
-        xi.xi_ecuacion = request.POST['xi_valores']
-        xi.save()
-        
-        fi.fi_ecuacion = request.POST['fi_valores']
-        fi.save()
-        
-        return HttpResponse("Guardado correctamente")
+        # Validar y convertir los datos a enteros (puedes manejar excepciones aquí si no son números válidos)
+        try:
+            xi = int(xi)
+            fi = int(fi)
+            resultado = xi + fi  # Realizar la operación de suma
+        except ValueError:
+            resultado = "Error: Por favor ingrese números válidos"
 
-    return render(request, 'index.html', {'xi_ecuacion': xi.xi_ecuacion, 'fi_ecuacion': fi.fi_ecuacion})
+    return render(request, 'index.html', {'resultado': resultado})
